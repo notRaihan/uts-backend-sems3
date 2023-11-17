10e780a25747c5412c67fa94e80b8866f1687f14
notRaihan <114415754+notRaihan@users.noreply.github.com>
2023-11-17 09:19:17 +0700

    feat: base code

---
+++ uts-backend-sems3/app/Models/employees_address.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class employees_address extends Model
+{
+    use HasFactory;
+
+    // call table employees_address
+    protected $table = 'employees_address';
+}
---
+++ uts-backend-sems3/app/Models/employees_contact.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class employees_contact extends Model
+{
+    use HasFactory;
+
+
+    // call table employees_contact
+    protected $table = 'employees_contact';
+    
+}
